81664abde5731fb4a8730584e316b666450abaf7
Shahab Uddin <shahab.82946@gmail.com>
2024-04-30 02:46:07 +0600

    added welcome note

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -80,7 +80,7 @@ const LoginPage = () => {
   <div className="hero-content flex-col lg:flex-row-reverse">
     <div className="text-center lg:text-left">
       <h1 className="text-5xl font-bold">Login now!</h1>
-      <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+      <p className="py-6">Welcome back to Woodland Crafts! Log in to your account to discover exclusive offers and more.</p>
     </div>
     <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
       <form onSubmit={handleLogin} className="card-body">
--- src/components/RegisterPage.jsx
+++ src/components/RegisterPage.jsx
@@ -67,7 +67,7 @@ const RegisterPage = () => {
   <div className="hero-content flex-col lg:flex-row-reverse">
     <div className="text-center lg:text-left">
       <h1 className="text-5xl font-bold">Register now!</h1>
-      <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+      <p className="py-6">Join us in celebrating the beauty of handmade Wooden Home decor and jute decor! Create an account to access exclusive offers and updates.</p>
     </div>
     <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
       <form onSubmit={handleRegister} className="card-body">
